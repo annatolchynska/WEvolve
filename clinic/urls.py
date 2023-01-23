f9d7bef52887a37094437861731ba3330e7c7fc2
annatolchynska <anna.t1417@gmail.com>
2023-01-23 10:41:43 +0000

    Make url paths and views.

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,6 +1,12 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', ),
+    path('', views.home, name="home"),
+    path('about.html', views.about, name='about'),
+    path('mental.html', views.mental, name='mental'),
+    path('holistic.html', views.holistic, name='holistic'),
+    path('addiction.html', views.addiction, name='addiction'),
+    path('costs.html', views.costs, name='costs'),
+
 ]
--- clinic/views.py
+++ clinic/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'index.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def mental(request):
+    return render(request, 'mental.html', {})
+
+
+def holistic(request):
+    return render(request, 'holistic.html', {})
+
+
+def addiction(request):
+    return render(request, 'addiction.html', {})
+
+
+def costs(request):
+    return render(request, 'costs.html', {})
