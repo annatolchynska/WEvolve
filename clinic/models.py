f4baddad5a000a7f6425e70113ba43b4cd0d6c78
annatolchynska <anna.t1417@gmail.com>
2023-01-27 16:45:41 +0000

    Create Appointment model.

--- clinic/models.py
+++ clinic/models.py
@@ -1,6 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from datetime import datetime
+
+'''
+TIME_CHOICES = (
+    ("9 AM", "9 AM"),
+    ("10 AM", "10 AM"),
+    ("11 AM", "11 AM"),
+    ("12 PM", "12 PM"),
+    ("1 PM", "1 PM"),
+    ("2 PM", "2 PM"),
+    ("3 PM", "3 PM"),
+    ("4 PM", "4 PM"),
+    ("5 PM", "5 PM"),
+    ("6 PM", "6 PM"),
+    ("7 PM", "7 PM"),
+)
+
+'''
 
 
-# Create your models here.
+class Appointment(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    sent_date = models.DateField(auto_now_add=True)
+    accepted = models.BooleanField(default=False)
+    accepted_date = models.DateField(auto_now_add=False, null=True, blank=True)
+    '''
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="9 AM")
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+'''
+
+    def __str__(self):
+        return self.first_name
